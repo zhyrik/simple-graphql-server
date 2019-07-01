a8df17b6e2b1d75bfe315a27a587389e2f83d503
zhurik <zhyriki@ukr.net>
2019-07-01 22:26:58 +0300

    complit queris

--- schema2.js
+++ schema2.js
@@ -7,28 +7,59 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
 } = graphql;
 
-const UseType = new GraphQLObjectType({
-  name: 'User',
-  fields: {
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
     id: { type: GraphQLString },
-    firsName: { type:  GraphQLString },
-    age: { type:  GraphQLInt }
-  }
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/company/${parentValue.id}/users`)
+          .then(res => res.data)
+      }
+    }
+  })
+});
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/company/${parentValue.companyId}`)
+          .then(res => res.data);
+      }
+    }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     user: {
-      type: UseType,
+      type: UserType,
       args: { id: { type: GraphQLString } },
-      resolve(parenValue, args) {
-        // request to db.json
+      resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`)
-          .then(resp => resp.data); // { data: {firstName: 'fjksa'}}
+          .then(resp => resp.data);
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/company/${args.id}`)
+          .then(resp => resp.data);
       }
     }
   }
--- steps.js
+++ steps.js
@@ -5,5 +5,9 @@
  * 4) створємо рут RootQuery в schema.js
  * 5) добавляємо resolve() {} в RootQuery
  * 6) пілключаємо json сервер сворюємо базу db.json і запускаємо json-server --watch db.json
- * 7)
+ * 7) створюю схеме2 з axios і конект schema2 з json-server
+ * 8) добавляємо nodemon
+ * 9) добавляю "companies" в db.json
+ * 10) add CompanyType at schema2.js
+ * 11) add company to RootQuery 
  */
